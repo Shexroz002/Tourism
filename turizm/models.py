2804fa88e5c518d9f226efba099371b8d0a98468
Shexroz002 <sh.toshpolatov@tuit.uz>
2022-10-19 12:07:38 +0500

    'add_verbase_name'

--- turizm/models.py
+++ turizm/models.py
@@ -4,16 +4,16 @@ from django.db import models
 # Create your models here.
 
 class AboutImages(models.Model):
-    photo_name = models.CharField(max_length = 30,default='')
-    photo_file = models.ImageField(upload_to = 'about_image/',null=False,blank=False)
+    photo_name = models.CharField(max_length = 30,default='',verbose_name = "About pagega chiqadigan rasm nomi")
+    photo_file = models.ImageField(upload_to = 'about_image/',null=False,blank=False,verbose_name = "About pagega chiqadigan rasm files")
     added_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.photo_name
 
 
 class AboutPage(models.Model):
-    title = models.CharField(max_length = 10000,default='',blank=False)
-    photo_files = models.ManyToManyField(AboutImages,related_name='photo_files',blank=True)
+    title = models.CharField(max_length = 10000,default='',blank=False,verbose_name = "About Pageda chiqadigan ma'limot")
+    photo_files = models.ManyToManyField(AboutImages,related_name='photo_files',blank=True,verbose_name = "About Pagega chiqadigan rasmlarni tanlang")
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -21,19 +21,19 @@ class AboutPage(models.Model):
 
 
 class OpenAndCloseHour(models.Model):
-    open_hour = models.TimeField()
-    close_hour = models.TimeField()
-    opan_close_day = models.CharField(max_length = 70)
+    open_hour = models.TimeField(verbose_name = "Mehmonhonani ochilish vaqti")
+    close_hour = models.TimeField(verbose_name = "Mehmonhonani yopilish vaqti")
+    opan_close_day = models.CharField(max_length = 70,default='Monday to Friday',verbose_name = "Mehmonhonani ishlash kunlari")
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.opan_close_day
 
 class Rooms(models.Model):
-    room_name = models.CharField(max_length = 250)
-    room_title = models.CharField(max_length = 1000)
-    room_price = models.FloatField()
-    room_file = models.ImageField(upload_to = 'room_image/',null=False,blank=False)
+    room_name = models.CharField(max_length = 250,verbose_name = "Xonani nomi")
+    room_title = models.CharField(max_length = 1000,verbose_name = "Xonani haqida malimot")
+    room_price = models.FloatField(verbose_name = "Xonani narxi")
+    room_file = models.ImageField(upload_to = 'room_image/',null=False,blank=False,verbose_name = "Xonani rasmi")
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -45,10 +45,10 @@ class MealChoice(models.IntegerChoices):
     DESSERT = 1, 'DESSERT'
     
 class MealModel(models.Model):
-    meal_name = models.CharField(max_length = 100)
-    meal_title = models.CharField(max_length = 100)
-    meal_price = models.FloatField()
-    user_role = models.IntegerField(choices=MealChoice.choices, default=MealChoice.HOTDISH)
+    meal_name = models.CharField(max_length = 100,verbose_name = "Nomi")
+    meal_title = models.CharField(max_length = 100,verbose_name = "haqida malimot")
+    meal_price = models.FloatField(verbose_name = "Narxi")
+    catagorya = models.IntegerField(choices=MealChoice.choices, default=MealChoice.HOTDISH,verbose_name = "Turi")
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -56,25 +56,25 @@ class MealModel(models.Model):
 
 
 class HotelRoomImage(models.Model):
-    hotel_room_image = models.ImageField(upload_to = 'hotel_room_image/',null=False,blank=False)
-    hotel_room_name = models.CharField(max_length = 100)
+    hotel_room_image = models.ImageField(upload_to = 'hotel_room_image/',null=False,blank=False,verbose_name = "Mehmon xona rasm")
+    hotel_room_name = models.CharField(max_length = 100,verbose_name = "Mehmonxona nomi")
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.hotel_room_name
 
 class CustomerMassage(models.Model):
-    name = models.CharField(max_length = 150,default='')
-    email = models.EmailField(max_length = 150,default='')
-    phone_nomer = models.IntegerField(default=0,null=False,blank=True)
-    message = models.CharField(max_length = 1000,default = '')
+    name = models.CharField(max_length = 150,default='',verbose_name = "Foydalanuvchi nomi")
+    email = models.EmailField(max_length = 150,default='',verbose_name = "Foydalanuvchi email")
+    phone_nomer = models.IntegerField(default=0,null=False,blank=True,verbose_name = "Foydalanuvchi  telefon nomeri")
+    message = models.CharField(max_length = 1000,default = '',verbose_name = "Foydalanuvchi yuborgan xabar")
     added_at = models.DateTimeField(auto_now_add=True)
 
 class HotelLocationModel(models.Model):
-    hotel_name =  models.CharField(max_length = 150,default='')
-    hotel_location =  models.CharField(max_length = 150,default='')
-    hotel_phone =  models.IntegerField(default=0,null=False,blank=True)
-    hotel_email =  models.EmailField(default=0,null=False,blank=True)
+    hotel_name =  models.CharField(max_length = 150,default='',verbose_name = "Nomi")
+    hotel_location =  models.CharField(max_length = 150,default='',verbose_name = "Joylashgan Hudud")
+    hotel_phone =  models.IntegerField(default=0,null=False,blank=True,verbose_name = "Telefon nomeri")
+    hotel_email =  models.EmailField(default=0,null=False,blank=True,verbose_name = "email")
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
